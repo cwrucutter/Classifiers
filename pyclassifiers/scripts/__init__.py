58dafe15e4c342407966872b13d588fba4eb9403
Andrew Wood <aew61@case.edu>
2016-05-25 21:57:11 -0400

    naming package "pyclassifiers" and using it in tests

---
+++ pyclassifiers/scripts/__init__.py
@@ -0,0 +1,1 @@
+from testFile import addValues
--- pyclassifiers/scripts/unitTest/testFile_unit.py
+++ pyclassifiers/scripts/unitTest/testFile_unit.py
@@ -3,17 +3,17 @@
 import unittest
 import rospy
 import rostest
-import scripts.testFile as f
+import pyclassifiers as p
 import sys
 
 print("System paths: %s" % sys.path)
 
 class addValuesTest(unittest.TestCase):
     def test_add_0_0(self):
-        self.assertEqual(0, f.addValues(0, 0))
+        self.assertEqual(0, p.addValues(0, 0))
 
     def test_add_1_3(self):
-        self.assertEqual(4, f.addValues(1, 3))
+        self.assertEqual(4, p.addValues(1, 3))
 
 if __name__ == "__main__":
     rostest.rosrun("pyclassifiers", "addValueTest", addValuesTest)

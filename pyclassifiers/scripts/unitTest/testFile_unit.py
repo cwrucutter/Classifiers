556bf823e32b96ec171be290e70b7403f25e3280
Andrew Wood <aew61@case.edu>
2016-05-25 21:43:36 -0400

    no coverage data generated
    need to import file under test

--- pyclassifiers/scripts/unitTest/testFile_unit.py
+++ pyclassifiers/scripts/unitTest/testFile_unit.py
@@ -3,14 +3,14 @@
 import unittest
 import rospy
 import rostest
-from scripts.testFile import addValues
+import scripts.testFile as f
 
 class addValuesTest(unittest.TestCase):
     def test_add_0_0(self):
-        self.assertEqual(0, addValues(0, 0))
+        self.assertEqual(0, f.addValues(0, 0))
 
     def test_add_1_3(self):
-        self.assertEqual(4, addValues(1, 3))
+        self.assertEqual(4, f.addValues(1, 3))
 
 if __name__ == "__main__":
     rostest.rosrun("pyclassifiers", "addValueTest", addValuesTest)

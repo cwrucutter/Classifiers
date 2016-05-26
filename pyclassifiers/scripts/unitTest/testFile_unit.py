e65d7dbdd5e865e8df1e22c5d94676aa5cb9b41d
Andrew Wood <aew61@case.edu>
2016-05-25 22:23:29 -0400

    manually invoking coverage

--- pyclassifiers/scripts/unitTest/testFile_unit.py
+++ pyclassifiers/scripts/unitTest/testFile_unit.py
@@ -5,8 +5,8 @@ import rospy
 import rostest
 import scripts.testFile as p
 import sys
-
-print("System paths: %s" % sys.path)
+import os
+from coverage import Coverage
 
 class addValuesTest(unittest.TestCase):
     def test_add_0_0(self):
@@ -16,4 +16,12 @@ class addValuesTest(unittest.TestCase):
         self.assertEqual(4, p.addValues(1, 3))
 
 if __name__ == "__main__":
+    cov = Coverage(
+        source=os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), ".."
+        )
+    )
+    cov.start()
     rostest.rosrun("pyclassifiers", "addValueTest", addValuesTest)
+    cov.stop()
+    cov.xml_report(outfile="-")

7cf22356ff160fc6899b1d4690a892d87c606f99
Andrew Wood <aew61@case.edu>
2016-05-25 20:51:27 -0400

    added rostest to package.xml
    listed rostest as a dependency, should resolve "ImportError (no module
    named rostest)" in python tests.

--- pyclassifiers/scripts/unitTest/testFile_unit.py
+++ pyclassifiers/scripts/unitTest/testFile_unit.py
@@ -5,7 +5,7 @@ import rospy
 import rostest
 from scripts.testFile import addValues
 
-class addValueTest(unittest.TestCase):
+class addValuesTest(unittest.TestCase):
     def test_add_0_0(self):
         self.assertEqual(0, addValues(0, 0))
 
@@ -13,4 +13,4 @@ class addValueTest(unittest.TestCase):
         self.assertEqual(4, addValues(1, 3))
 
 if __name__ == "__main__":
-    rostest.rosrun("pyclassifiers", "addValueTest", addValueTest)
+    rostest.rosrun("pyclassifiers", "addValueTest", addValuesTest)

7f58d54b792f70bf57d0bf24f256439ef5a2eb8a
Andrew Wood <aew61@case.edu>
2016-05-28 17:39:08 -0400

    resolving paths for imports of files in tests

--- pyclassifiers/scripts/unitTest/testFile_unit.py
+++ pyclassifiers/scripts/unitTest/testFile_unit.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python
 
+# SYSTEM IMPORTS
 import unittest
 import rospy
 import rostest
-import scripts.testFile as p
 import sys
 import os
 from coverage import Coverage
 
+
+
+currentDir = os.path.dirname(os.path.realpath(__file__))
+scriptsDir = os.path.join(currentDir, "..")
+sys.path.append([currentDir, scriptsDir])
+
+# PYTHON PROJECT IMPORTS
+import testFile
+
 class addValuesTest(unittest.TestCase):
     def test_add_0_0(self):
-        self.assertEqual(0, p.addValues(0, 0))
+        self.assertEqual(0, testFile.addValues(0, 0))
 
     def test_add_1_3(self):
-        self.assertEqual(4, p.addValues(1, 3))
+        self.assertEqual(4, testFile.addValues(1, 3))
 
 if __name__ == "__main__":
-    currentDir = os.path.dirname(os.path.realpath(__file__))
-    cov = Coverage(source=os.path.join(currentDir, ".."))
+    # currentDir = os.path.dirname(os.path.realpath(__file__))
+    cov = Coverage(source=scriptsDir)
     cov.start()
     rostest.rosrun("pyclassifiers", "addValueTest", addValuesTest)
     cov.stop()
